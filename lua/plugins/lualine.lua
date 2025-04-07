4042e5f71bb52e25ab2be7c3fe8376f6d3197cbe
dy4wd <teylor.neyll@gmail.com>
2025-04-07 18:44:04 +0400

    del symbols

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,8 +5,8 @@ return {
         options = {
             icons_enabled = false,
             theme = "auto",
-            component_separators = { left = "", right = "" },
-            section_separators = { left = "", right = "" },
+            component_separators = { left = "", right = "" },
+            section_separators = { left = "", right = "" },
             disabled_filetypes = {},
             always_divide_middle = true,
             globalstatus = false,
